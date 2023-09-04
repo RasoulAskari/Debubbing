25100835965391522c9e842d98b6151e54026172
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-04 16:52:11 +0500

    state table atributte

--- database/migrations/2023_09_04_055041_create_state_schemas_table.php
+++ database/migrations/2023_09_04_055041_create_state_schemas_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('state_schemas', function (Blueprint $table) {
             $table->id();
-
             $table->string("name");
             $table->string("state_code");
             $table->string("latitude");

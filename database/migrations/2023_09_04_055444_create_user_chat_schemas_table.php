afe02d55a543b39ef05cbc0255f4abd4bc315c9d
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-04 17:01:51 +0500

    user chat table atribute

--- database/migrations/2023_09_04_055444_create_user_chat_schemas_table.php
+++ database/migrations/2023_09_04_055444_create_user_chat_schemas_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('user_chat_schemas', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table
+                ->unsignedBigInteger("chat_id")->notNullable();
+            $table->foreign("chat_id")->references("id")->on("chat_schemas")->deferrable("deferred");
+            $table
+                ->uuid("user_id")
+                ->references("id")->on('user_schemas')
+                ->deferrable("deferred")
+                ->index("user_chat_user_id_index", "hash");
         });
     }
 

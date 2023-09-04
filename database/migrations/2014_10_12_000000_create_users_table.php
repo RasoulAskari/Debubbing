18f3a2cf64175b3084ca513695ad26f1b7c2d0f3
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-04 16:54:14 +0500

    user table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,14 +12,50 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string("full_name");
-            $table->string("email")->unique();
-            $table->string("phone_no")->unique();
-            $table->string("password");
+            $table
+                ->uuid("id")
+                ->index("user_id_index", "hash");
+            $table->string("full_name")->index("user_fullname_index", "btree");
+            $table->string("username")->index("user_username_index", "btree");
+            $table->string("phone_no")->index("user_phone_no_index", "btree");
+            $table->text("bio")->nullable();
+            $table->string("password")->nullable();
             $table->timestamp("birth_date")->nullable();
             $table->enum("gender", ["male", "female"]);
+            $table
+                ->enum("status", ["active", "suspended"])
+                ->default("active")
+                ->index("user_status_index", "hash");
+            $table
+                ->enum("profile_type", ["public", "private"])
+                ->default("public")
+                ->index("user_profile_type_index", "hash");
+
             $table->integer("profile_picture")->unsigned()->nullable();
+            $table->integer("cover_photo")->unsigned()->nullable();
+            $table->timestamp("phone_verified_at")->nullable();
+            $table->timestamp("suspended_until")->nullable();
+            $table->integer("suspended_id")->nullable();
+
+            $table
+                ->decimal("latitude", 14, 8)
+                ->nullable()
+                ->index("user_latitude_index", "hash");
+            $table
+                ->decimal("longitude", 14, 8)
+                ->nullable()
+                ->index("user_longitude_index", "hash");
+
+            $table->unsignedBigInteger("state_id")->nullable();
+            $table->foreign("state_id")->references("id")->on('state_schemas')->deferrable("deferred");
+            $table->boolean("verified")->default(false);
+            $table
+                ->uuid("verified_by")
+                ->references("administrators->id")
+                ->deferrable("deferred")
+                ->index("user_verified_by_index", "hash");
+            $table->timestamp("last_online")->nullable();
+
             $table->rememberToken();
             $table->softDeletes();
             $table->timestamps();

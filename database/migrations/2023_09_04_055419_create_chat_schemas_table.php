ec3f84d76959a7f22bab8d5443a01977f6edeff3
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-04 17:01:27 +0500

    chat table atribute

--- database/migrations/2023_09_04_055419_create_chat_schemas_table.php
+++ database/migrations/2023_09_04_055419_create_chat_schemas_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('chat_schemas', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->boolean("is_accepted")->defaultTo(true);
+            $table->timestamp("last_messaged_at");
+            $table->json("recipients")->nullable();
+
             $table->timestamps();
         });
     }

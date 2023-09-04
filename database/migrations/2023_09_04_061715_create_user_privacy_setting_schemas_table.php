db9114296ca031932591951d7288741a616649e8
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-04 17:27:28 +0500

    user privacy table atribute

--- database/migrations/2023_09_04_061715_create_user_privacy_setting_schemas_table.php
+++ database/migrations/2023_09_04_061715_create_user_privacy_setting_schemas_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('user_privacy_setting_schemas', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table
+                ->uuid("user_id")
+                ->references("users.id")
+                ->deferrable("deferred")
+                ->index("user_privacy_user_id_index", "hash");
+            $table
+                ->enum("privacy_type", ["follow_list", "profile_privacy"])
+                ->index("user_privacy_type_index", "hash");
+            $table->string("privacy_value")->index("user_privacy_value_index", "hash");
         });
     }
 

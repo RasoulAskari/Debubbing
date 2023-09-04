5c69ad99fb9b0364fa9c61f1a23fea146555f083
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-04 16:57:54 +0500

    attachment table atribute

--- database/migrations/2023_09_04_055313_create_attachment_schemas_table.php
+++ database/migrations/2023_09_04_055313_create_attachment_schemas_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('attachment_schemas', function (Blueprint $table) {
             $table->id();
+            $table->string("url")->index("attachment_url_index", "btree")->notNullable();
+            $table->integer("height")->index("attachment_height_index", "hash")->nullable();
+            $table->integer("width")->index("attachment_width_index", "hash")->nullable();
+            $table->string("duration")->nullable();
+            $table->string("mime_type")->index("attachment_mime_type_index", "hash");
+            $table->integer("size")->nullable();
+            $table->enum("uploaded_by_type", ["admin", "user"]);
+            $table->uuid("uploaded_by")->index("attachment_uploaded_by_index", "hash");
+
             $table->timestamps();
         });
     }

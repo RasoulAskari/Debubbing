cb9d09306843816ce57be7c1db8dee8aae5e63c3
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-04 16:55:24 +0500

    post table aribute

--- database/migrations/2023_09_04_055154_create_post_schemas_table.php
+++ database/migrations/2023_09_04_055154_create_post_schemas_table.php
@@ -13,6 +13,24 @@ return new class extends Migration
     {
         Schema::create('post_schemas', function (Blueprint $table) {
             $table->id();
+            $table->text("content")->index("post_content_index", "btree")->nullable();
+            $table->enum("privacy", ["public", "followers", "only_me"])->nullable();
+            $table->enum("post_type", ["profile", "cover", "share", "post"])->nullable();
+            $table
+                ->enum("status", ["published", "suspended"])
+                ->index("post_status_index", "hash")
+                ->default("published");
+
+            $table->enum("attachment_type", ["none", "img", "video"])->nullable();
+            $table
+                ->uuid("created_by")
+                ->references("id")->on('user_schemas ')
+                ->deferrable("deferred")
+                ->nullable()
+                ->onDelete("SET NULL")
+                ->index()
+                ->index("post_created_by_index", "hash");
+
             $table->timestamps();
         });
     }

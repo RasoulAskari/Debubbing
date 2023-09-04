f111b8f496d7d4205d663fe447ac1d67b1a7cfe6
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-04 17:14:18 +0500

    tash tag table atribute

--- database/migrations/2023_09_04_060937_create_post_hash_tag_schemas_table.php
+++ database/migrations/2023_09_04_060937_create_post_hash_tag_schemas_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('post_hash_tag_schemas', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+
+            $table->string("title")->index("hashtag_title_index", "hash");
+            $table->timestamps(true, true);
         });
     }
 

777aa5928ab31e7452fb39546b894c7c2acb4b98
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-04 17:16:57 +0500

    changes

--- database/migrations/2023_09_04_060937_create_post_hash_tag_schemas_table.php
+++ database/migrations/2023_09_04_060937_create_post_hash_tag_schemas_table.php
@@ -14,7 +14,13 @@ return new class extends Migration
         Schema::create('post_hash_tag_schemas', function (Blueprint $table) {
             $table->id();
 
-            $table->string("title")->index("hashtag_title_index", "hash");
+            $table
+                ->unsignedBigInteger("post_id");
+            $table->foreign("post_id")->references("id")->on('post_schemas')->deferrable("deferred");
+            $table
+                ->unsignedBigInteger("hash_tag_id")
+                ->nullable();
+            $table->foreign("hash_tag_id")->references("id")->on('post_schemas')->deferrable("deferred");
             $table->timestamps(true, true);
         });
     }

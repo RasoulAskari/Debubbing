5333e9df1388a24547af88167044ac1b78267404
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-04 17:13:39 +0500

    shared post table atribute

--- database/migrations/2023_09_04_060901_create_shared_post_schemas_table.php
+++ database/migrations/2023_09_04_060901_create_shared_post_schemas_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('shared_post_schemas', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+
+            $table
+                ->unsignedBigInteger("shared_post");
+            $table->foreign("shared_post")->references("id")->on('post_schemas')->deferrable("deferred");
         });
     }
 

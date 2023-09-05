e32c492455664d7d57ac78111623db52264b3621
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-05 09:43:29 +0500

    Admin Factory atributes

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Admin>
@@ -17,7 +18,14 @@ class AdminFactory extends Factory
     public function definition(): array
     {
         return [
-            
+            'full_name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone_no' => fake()->phoneNumber(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'birth_date' => fake()->dateTime(),
+            'gender' => fake()->randomElement(['male', 'female']),
+            'remember_token' => Str::random(10),
         ];
     }
 }

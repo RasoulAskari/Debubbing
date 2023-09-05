3030e7021d7ea9d0d01a5d11bad842da82052f42
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-05 09:32:32 +0500

    changes

--- database/migrations/2023_09_03_144603_create_admins_table.php
+++ database/migrations/2023_09_03_144603_create_admins_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('admins', function (Blueprint $table) {
-
             $table->id();
             $table->string("full_name");
             $table->string("email")->unique();

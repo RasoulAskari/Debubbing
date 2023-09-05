37986c4cf59bfeb83732857636e6f375525346e7
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-05 19:58:17 +0500

    remove email from create user table

--- database/migrations/2023_09_04_055105_create_users_table.php
+++ database/migrations/2023_09_04_055105_create_users_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string("username")->index("user_username_index", "btree");
             $table->string("phone_no")->index("user_phone_no_index", "btree");
             $table->text("bio")->nullable();
-            $table->string("email")->unique();
             $table->string("password")->nullable();
             $table->timestamp("birth_date")->nullable();
             $table->enum("gender", ["male", "female"]);
